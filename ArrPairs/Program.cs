de3193c4b395f5d16fc7f256fb68c64fab97836e
Dan-Mihai Petrus <34897927+danpetrus@users.noreply.github.com>
2021-02-17 13:01:35 +0200

    Add files via upload

---
+++ ArrPairs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ArrPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Nums = { 1, 2, 3 };
+            int x = 0;
+            int i = 0;
+            bool leastOne = false;
+
+            while (i < Nums.Length)
+            { 
+                x = i;
+                while (x < Nums.Length)
+                {
+                    if (Nums[i] == Nums[x] && x != i)
+                    {
+                        Console.Write("(" + i + "," + x + ") ");
+                        leastOne = true;
+                    }
+                x++;
+                }
+            i++;
+            }
+
+            if (leastOne == false)
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
